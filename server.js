6d86d18ddd9de840ceaf119d5d564dcd580f9287
mikaylawhiteaker <mikayla.whiteaker@gmail.com>
2018-09-26 11:49:24 -0600

    change server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/LSST_Labeler'));
+app.use(express.static(__dirname + '/dist/LSST-Labeler'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/LSST_Labeler/index.html'));
+res.sendFile(path.join(__dirname+'/dist/LSST-Labeler/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
