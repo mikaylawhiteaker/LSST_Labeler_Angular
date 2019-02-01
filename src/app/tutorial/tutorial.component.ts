c19dd2b537c6f3bd0d79e29b4e80f7d5f1063f64
dcarbonari15 <carbonar19@up.edu>
2019-02-01 12:38:53 -0800

    tutorial functionality
    Added 4 more pop-up series to the tutorial and added functionality to force the user to step through the pop up windows in order with the option to go back to previous popups after they have been viewed.

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -17,6 +17,12 @@ export class TutorialComponent {
   animal: string;
   name: string;
   page: number = 0;
+  show1: boolean = false;
+  show2: boolean = false;
+  show3: boolean = false;
+  show4: boolean = false;
+  show: boolean = false;
+  showintro: boolean = true;
 
 
   constructor(public dialog: MatDialog) {
@@ -36,7 +42,63 @@ export class TutorialComponent {
       console.log('The dialog was closed');
       this.animal = result;
     });
+    this.show1 = true;
+    this.show = true;
+    this.showintro = false;
   }
+
+  openDialog1(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogTutorial1, {
+      width: '300px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+    this.show2 = true;
+  }
+
+  openDialog2(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogTutorial2, {
+      width: '300px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+    this.show3 = true;
+  }
+
+  openDialog3(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogTutorial3, {
+      width: '300px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+    this.show4 = true;
+  }
+
+  openDialog4(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogTutorial4, {
+      width: '300px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+    this.show4 = true;
+  }
+
 }
 
 @Component({
@@ -82,8 +144,184 @@ export class DialogOverviewExampleDialogTutorial {
       this.dialogRef.close();
     }
   }
+}
 
+@Component({
+  selector: 'dialog-overview-example-dialog-tutorial-1',
+  templateUrl: 'dialog-overview-example-dialog-tutorial-1.html',
+  styleUrls: ['./tutorial.component.css']
+})
+export class DialogOverviewExampleDialogTutorial1 {
+  titles : string[] = [
+    "Welcome to the LSST Data Labeler tutorial part 2",
+    "insert part 2 page 2 data here",
+    "insert part 2 page 3 data here",
+    "insert part 2 page 4 data here",
+    "insert part 2 page 5 data here",
+  ];
+  contents : string[] = [
+    "insert part 2 page 1 data here",
+    "insert part 2 page 2 data here",
+    "insert part 2 page 3 data here",
+    "insert part 2 page 4 data here",
+    "insert part 2 page 5 data here",
+  ];
+  page : number = 0;
+  buttonText : string;
+  isClose : boolean = false;
 
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogTutorial>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+      this.buttonText = "Next";
+    }
 
+  nextPage(): void {
+    this.page++;
+    if(this.isClose) {
+      this.dialogRef.close();
+    }
+    if(this.page == 4) {
+      this.buttonText = "Close";
+      this.isClose = true;
+    }
+    if(this.page > 4) {
+      this.dialogRef.close();
+    }
+  }
+}
 
+@Component({
+  selector: 'dialog-overview-example-dialog-tutorial-2',
+  templateUrl: 'dialog-overview-example-dialog-tutorial-2.html',
+  styleUrls: ['./tutorial.component.css']
+})
+export class DialogOverviewExampleDialogTutorial2 {
+  titles : string[] = [
+    "Welcome to the LSST Data Labeler tutorial part 3",
+    "insert part 3 page 2 data here",
+    "insert part 3 page 3 data here",
+    "insert part 3 page 4 data here",
+    "insert part 3 page 5 data here",
+  ];
+  contents : string[] = [
+    "insert part 3 page 1 data here",
+    "insert part 3 page 2 data here",
+    "insert part 3 page 3 data here",
+    "insert part 3 page 4 data here",
+    "insert part 3 page 5 data here",
+  ];
+  page : number = 0;
+  buttonText : string;
+  isClose : boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogTutorial>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+      this.buttonText = "Next";
+    }
+
+  nextPage(): void {
+    this.page++;
+    if(this.isClose) {
+      this.dialogRef.close();
+    }
+    if(this.page == 4) {
+      this.buttonText = "Close";
+      this.isClose = true;
+    }
+    if(this.page > 4) {
+      this.dialogRef.close();
+    }
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog-tutorial-3',
+  templateUrl: 'dialog-overview-example-dialog-tutorial-3.html',
+  styleUrls: ['./tutorial.component.css']
+})
+export class DialogOverviewExampleDialogTutorial3 {
+  titles : string[] = [
+    "Welcome to the LSST Data Labeler tutorial part 4",
+    "insert part 4 page 2 data here",
+    "insert part 4 page 3 data here",
+    "insert part 4 page 4 data here",
+    "insert part 4 page 5 data here",
+  ];
+  contents : string[] = [
+    "insert part 4 page 1 data here",
+    "insert part 4 page 2 data here",
+    "insert part 4 page 3 data here",
+    "insert part 4 page 4 data here",
+    "insert part 4 page 5 data here",
+  ];
+  page : number = 0;
+  buttonText : string;
+  isClose : boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogTutorial>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+      this.buttonText = "Next";
+    }
+
+  nextPage(): void {
+    this.page++;
+    if(this.isClose) {
+      this.dialogRef.close();
+    }
+    if(this.page == 4) {
+      this.buttonText = "Close";
+      this.isClose = true;
+    }
+    if(this.page > 4) {
+      this.dialogRef.close();
+    }
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog-tutorial-4',
+  templateUrl: 'dialog-overview-example-dialog-tutorial-4.html',
+  styleUrls: ['./tutorial.component.css']
+})
+export class DialogOverviewExampleDialogTutorial4 {
+  titles : string[] = [
+    "Welcome to the LSST Data Labeler tutorial part 5",
+    "insert part 5 page 2 data here",
+    "insert part 5 page 3 data here",
+    "insert part 5 page 4 data here",
+    "insert part 5 page 5 data here",
+  ];
+  contents : string[] = [
+    "insert part 5 page 1 data here",
+    "insert part 5 page 2 data here",
+    "insert part 5 page 3 data here",
+    "insert part 5 page 4 data here",
+    "insert part 5 page 5 data here",
+  ];
+  page : number = 0;
+  buttonText : string;
+  isClose : boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogTutorial>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+      this.buttonText = "Next";
+    }
+
+  nextPage(): void {
+    this.page++;
+    if(this.isClose) {
+      this.dialogRef.close();
+    }
+    if(this.page == 4) {
+      this.buttonText = "Close";
+      this.isClose = true;
+    }
+    if(this.page > 4) {
+      this.dialogRef.close();
+    }
+  }
 }
