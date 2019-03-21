2f04513272e2cfac662a9e668b77390e53d6b1c0
mikaylawhiteaker <mikayla.whiteaker@gmail.com>
2019-03-20 19:04:42 -0600

    labeling works

--- src/app/space-image/space-image.component.ts
+++ src/app/space-image/space-image.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 declare var JS9: any;
+declare var X: any;
+declare var Y: any;
 
 @Component({
   selector: 'app-space-image',
@@ -11,28 +13,50 @@ declare var JS9: any;
 export class SpaceImageComponent implements OnInit {
 
   label: string;
-  x: number;
-  y: number;
+  x_coord: number;
+  y_coord: number;
+  url: string;
+  info: string;
 
   constructor(private http: HttpClient) {
+    this.info = "Is the source in the green circle a star?"
+    this.url = "../assets/img/patch_1_1.fits";
     this.getSpaceImage()
       .subscribe( (data: SpaceImage)  => {
         this.label = JSON.parse(data.body)['label'];
-        this.x = JSON.parse(data.body)['coords'][0];
-        this.y = JSON.parse(data.body)['coords'][1];
+        this.x_coord = JSON.parse(data.body)['coords'][0];
+        this.y_coord = JSON.parse(data.body)['coords'][1];
+        X = this.x_coord;
+        Y = this.y_coord;
         console.log(this.label);
-        console.log(this.x);
-        console.log(this.y);
-        var image_url = "https://s3-us-west-2.amazonaws.com/lsst-actual/843df07d-4760-46d3-9ef4-433df6a11fd7.fits"
-        var opts = {scale: "asinh"}
-        JS9.Preload("../assets/img/patch_1_1.fits", opts);
+        console.log(this.x_coord);
+        console.log(this.y_coord);
+
+        let imageSetUp = function(x, y) {
+          console.log(JS9.GetLoadStatus(this.url));
+          // JS9.SetZoom("toFit");
+          JS9.SetWCS('A');
+          JS9.SetWCSUnits("pixels");
+          JS9.SetScale("asinh", -1, 30);
+          console.log(X);
+          console.log(Y);
+          // console.log(JS9.GetWCSSys());
+          // var pixobj = JS9.WCSToPix(X, Y);
+          // console.log(pixobj);
+          JS9.SetPan({px:(X-1), py:(Y-1)});
+          JS9.AddRegions("circle", {px:(X-1), py:(Y-1)});
+        };
+
+        let displayFull = function() {
+          var opts = {xdim:4200, ydim:4200, ondisplaysection: imageSetUp}
+          JS9.DisplaySection(opts);
+        }
+
+        var opts = {onload: displayFull}
+
+        JS9.Load(this.url, opts);
         console.log(JS9);
-        // JS9.SetScale("asinh", -1, 30);
-        // JS9.SetZoom("in");
-        // console.log(JS9.GetZoom())
-        // JS9.PixToWCS();
-        // JS9.SetPan(this.x, this.y);
-        // JS9.AddRegions("circle", {x: this.x, y:this.y, radius: 15, color: "green"});
+
       });
   }
 
@@ -45,17 +69,41 @@ export class SpaceImageComponent implements OnInit {
 
   LabeledYes(){
     console.log("you labeled yes");
+    if(this.label == "STAR") {
+      this.info = "Correct! The source is a star.";
+    } else {
+      this.info = "Not Correct. The source is not a star.";
+    }
   }
 
   LabeledNo(){
       console.log("you labeled no");
+      if(this.label != "STAR") {
+        this.info = "Correct! The source is not a star.";
+      } else {
+        this.info = "Not Correct. The source is a star.";
+      }
   }
 
-  // NextImage(){
-  //   console.log("you went to next image");
-  //     this.getSpaceImage()
-  //       .subscribe( (data: SpaceImage)  => this.link = JSON.parse(data.body)['url']);
-  // }
+  NextImage(){
+    console.log("you went to next image");
+      this.getSpaceImage()
+        .subscribe( (data: SpaceImage)  => {
+            this.label = JSON.parse(data.body)['label'];
+            this.x_coord = JSON.parse(data.body)['coords'][0];
+            this.y_coord = JSON.parse(data.body)['coords'][1];
+            X = this.x_coord;
+            Y = this.y_coord;
+            console.log(this.label);
+            console.log(this.x_coord);
+            console.log(this.y_coord);
+
+            JS9.RemoveRegions("all");
+            JS9.SetPan({px:(X-1), py:(Y-1)});
+            JS9.AddRegions("circle", {px:(X-1), py:(Y-1)});
+
+        });
+  }
 }
 
 class SpaceImage {
